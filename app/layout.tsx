6ad6ebace666f27956fe81a1903862c50ad2e2fc
Mirkomilboy <msadirov1997@gmail.com>
2025-09-08 06:17:27 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Mirkomilboy's Portfolio",
+  description:
+    "Passionate Frontend Engineer, high-performance web experiences with NextJS, ReactJS, Tailwind CSS and many other tools.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/M.svg" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/RecentProjects.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { projects } from "@/data";
+import { PinContainer } from "./ui/Pin";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import Image from "next/image";
+import Link from "next/link";
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+
+const RecentProjects = () => {
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
+      </h1>
+      <div className="max-w-6xl mx-auto grid grid-cols-2 p-4 gap-16 mt-10">
+        {projects.map((item) => (
+          <div
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center"
+            key={item.id}
+          >
+            <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  {item.title}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  {item.des}
+                </CardItem>
+                <CardItem translateZ="100" className="w-full mt-4">
+                  <Image
+                    src={item.img}
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="flex items-center"
+                  >
+                    <AnimatedTooltip items={item.iconLists} />
+                  </CardItem>
+                  <CardItem
+                    translateZ={20}
+                    as={Link}
+                    href="https://twitter.com/mannupaaji"
+                    target="__blank"
+                    className="flex justify-center items-center"
+                  >
+                    <p className="flex text-sm md:text-sm text-purple">
+                      Check Live Site
+                    </p>
+                    <FaLocationArrow className="ms-3" color="#CBACF9" />
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecentProjects;
